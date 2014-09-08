010c3121f445ad3338b178d3018c4a9dd23501aa
Matthew Boston <matthew.boston@gmail.com>
2014-09-08 17:22:15 -0600

    test defined :okta-config option

--- test/ring/middleware/okta_test.clj
+++ test/ring/middleware/okta_test.clj
@@ -4,9 +4,20 @@
             [ring.mock.request :refer [request]]
             [ring.util.response :refer [response]]))
 
+(def okta-home "https://company.okta.com")
+(def okta-config "/test-resources/custom-okta-config.xml")
+
 (deftest test-wrap-okta
-  (testing ":okta-home option is required"
-    (let [default-handler #(response %)]
+  (let [default-handler #(response %)]
+    (testing ":okta-home option is required"
       (let [handler (wrap-okta default-handler)]
         (is (thrown? IllegalArgumentException
-                     (handler (request :get "/"))))))))
+                     (handler (request :get "/"))))))
+
+    (testing "#login"
+      (testing "default :okta-config")
+      (testing "defined :okta-config"
+        (let [handler (wrap-okta default-handler {:okta-home okta-home
+                                                  :okta-config okta-config})
+              response (handler (request :post "/login"))]
+          (is (= (-> response :body :okta-config-location) okta-config)))))))
