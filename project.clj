333b05d58153ce0bd52f542671272d6013a625ea
Matthew Boston <matthew.boston@gmail.com>
2014-09-10 11:17:58 -0600

    0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-okta "0.1.1"
+(defproject ring-okta "0.1.2-SNAPSHOT"
   :description "Ring middleware for Okta Single Sign-on"
   :url "https://github.com/Hendrick/ring-okta"
   :license {:name "Eclipse Public License"
