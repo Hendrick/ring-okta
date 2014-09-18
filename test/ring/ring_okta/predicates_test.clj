3a3c13243da519b74cfc42821a5ce71e5f24cb1b
Matthew Boston <matthew.boston@gmail.com>
2014-09-18 17:15:49 -0600

    tests for predicates

---
+++ test/ring/ring_okta/predicates_test.clj
@@ -0,0 +1,40 @@
+(ns ring.ring-okta.predicates-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [ring.ring-okta.predicates :as p]
+            [ring.mock.request :refer [request]]))
+
+(deftest test-login?
+  (testing "match method and route"
+    (is (p/login? (request :post "/login"))))
+
+  (testing "method but not route"
+    (is (not (p/login? (request :post "/foo")))))
+
+  (testing "match route but not method"
+    (is (not (p/login? (request :get "/login"))))))
+
+(deftest test-logout?
+  (testing "match method and route"
+    (is (p/logout? (request :post "/logout"))))
+
+  (testing "match method but not route"
+    (is (not (p/logout? (request :post "/foo")))))
+
+  (testing "match route but not method"
+    (is (not (p/logout? (request :get "/logout"))))))
+
+(deftest test-logged-in?
+  ;; TODO
+  )
+
+(deftest test-skip-route?
+  (is (p/skip-route? (request :get "/foo")
+                     [:get "/foo" :post "/foo"]))
+  (is (not (p/skip-route? (request :get "/foo")
+                          [:get "/bar"])))
+  (is (not (p/skip-route? (request :get "/foo")
+                          [:post "/foo"]))))
+
+(deftest test-force-user?
+  ;; TODO
+  )
