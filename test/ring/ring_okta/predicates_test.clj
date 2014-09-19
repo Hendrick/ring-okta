34b11812f59d3579c93d905d21ce27ff30dc240b
Matthew Boston <matthew.boston@gmail.com>
2014-09-19 10:47:15 -0600

    is-not testing macro

--- test/ring/ring_okta/predicates_test.clj
+++ test/ring/ring_okta/predicates_test.clj
@@ -3,33 +3,37 @@
             [ring.ring-okta.predicates :as p]
             [ring.mock.request :refer [request]]))
 
+(defmacro is-not
+  ([form] `(is-not ~form nil))
+  ([form msg] `(is (not ~form) ~msg)))
+
 (deftest test-login?
   (testing "match method and route"
     (is (p/login? (request :post "/login"))))
 
   (testing "match method but not route"
-    (is (not (p/login? (request :post "/foo")))))
+    (is-not (p/login? (request :post "/foo"))))
 
   (testing "match route but not method"
-    (is (not (p/login? (request :get "/login"))))))
+    (is-not (p/login? (request :get "/login")))))
 
 (deftest test-logout?
   (testing "match method and route"
     (is (p/logout? (request :post "/logout"))))
 
   (testing "match method but not route"
-    (is (not (p/logout? (request :post "/foo")))))
+    (is-not (p/logout? (request :post "/foo"))))
 
   (testing "match route but not method"
-    (is (not (p/logout? (request :get "/logout"))))))
+    (is-not (p/logout? (request :get "/logout")))))
 
 (deftest test-skip-route?
   (is (p/skip-route? (request :get "/foo")
                      [:get "/foo" :post "/foo"]))
-  (is (not (p/skip-route? (request :get "/foo")
-                          [:get "/bar"])))
-  (is (not (p/skip-route? (request :get "/foo")
-                          [:post "/foo"])))
+  (is-not (p/skip-route? (request :get "/foo")
+                         [:get "/bar"]))
+  (is-not (p/skip-route? (request :get "/foo")
+                         [:post "/foo"]))
 
   (testing ":any can be used to match all route methods"
     (is (p/skip-route? (request :get "/foo")
