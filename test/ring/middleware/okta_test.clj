c11678c5cc9594339f3566f923f5f1c8e4e0f192
Matthew Boston <matthew.boston@gmail.com>
2014-09-24 21:53:04 -0600

    test :force-user option

--- test/ring/middleware/okta_test.clj
+++ test/ring/middleware/okta_test.clj
@@ -63,5 +63,15 @@
               response (handler (request :get "/foo"))]
           (is (nil? (-> response :body :uri))))))
 
-    (testing "#force-user")
+    (testing "#force-user"
+      (testing "with :force-user defined"
+        (let [handler (wrap-okta default-handler {:okta-home okta-home
+                                                  :force-user "foo@bar.com"})
+              response (handler (request :get "/foo"))]
+          (is (= "foo@bar.com" (-> response :body :session :okta/user)))))
+
+      (testing "without :force-user defined"
+        (let [handler (wrap-okta default-handler {:okta-home okta-home})
+              response (handler (request :get "/foo"))]
+          (is (nil? (-> response :body :session :okta/user))))))
     (testing "#other")))
