a9b5ebbab7dc5e75942d4e28ed2b0861817d2ec1
Matthew Boston <matthew.boston@gmail.com>
2014-09-25 13:39:14 -0600

    test login adds user to session

---
+++ test/ring/ring_okta/session_test.clj
@@ -0,0 +1,21 @@
+(ns ring.ring-okta.session-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [clojure.java.io :as io]
+            [ring.ring-okta.session :refer [login]]))
+
+(defn- stub-respond-to-okta-post [params okta-config]
+  {:redirect-url "http://foo.bar.com"
+   :authenticated-user-email "foo@bar.com"})
+
+(defn- stub-redirect-after-post [url]
+  {})
+
+(deftest test-login
+  (testing "user placed in session"
+    (let [request {:params {}
+                   :okta-config-location (io/resource "okta-config.xml")}]
+      (with-redefs [ring.ring-okta.session/respond-to-okta-post stub-respond-to-okta-post
+                    ring.util.response/redirect-after-post stub-redirect-after-post]
+        (is (= "foo@bar.com" (-> (login request) :session :okta/user))))))
+
+  (testing "redirect after login"))
