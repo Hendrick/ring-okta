e5511c1ddc5ef31f4a414a6bc08ea66fca64aae4
Matthew Boston <matthew.boston@gmail.com>
2015-12-01 16:08:34 -0700

    update saml-toolkit 1.0.12-000170-c7ed721

--- project.clj
+++ project.clj
@@ -10,17 +10,19 @@
                  [ring-mock "0.1.5" :scope "test"]
                  [compojure "1.4.0"]
                  [org.clojure/data.codec "0.1.0"]
-                 [com.okta/saml-toolkit "1.0.5-000148-30924be" :exclusions [[org.slf4j/slf4j-api]]]
+                 [com.okta/saml-toolkit "1.0.12-000170-c7ed721"]
 
                  ;; okta dependencies -- some are not specified in their pom,
                  ;; others are borked because of our weird local repo thing that
                  ;; we do in order to please the Travis-CI gods
                  [com.sun.xml.parsers/jaxp-ri "1.4.5"]
+                 [org.slf4j/slf4j-api "1.6.1" :scope "provided"]
+                 [org.slf4j/slf4j-simple "1.6.1" :scope "provided"]
                  [com.google.inject/guice "3.0"]
                  [org.bouncycastle/bcprov-jdk16 "1.45"]
                  [org.apache.commons/commons-lang3 "3.0"]
                  [javax.servlet/javax.servlet-api "3.0.1" :scope "provided"]
-                 [org.opensaml/opensaml "2.5.1-1"]]
+                 [org.opensaml/opensaml "2.6.4"]]
   :plugins [[codox "0.8.12"]
             [lein-cloverage "1.0.6"]]
   :codox {:include ring.middleware.okta
