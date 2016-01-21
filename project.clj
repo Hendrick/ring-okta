0334cb6756386813bfcc64fb0a6166ae070d8e6d
Matthew Boston <matthew.boston@gmail.com>
2016-01-21 08:31:01 -0700

    add clojure 1.8 profile

--- project.clj
+++ project.clj
@@ -33,7 +33,9 @@
              :1.6 {:resource-paths ["test-resources"]
                    :dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:resource-paths ["test-resources"]
-                   :dependencies [[org.clojure/clojure "1.7.0"]]}}
-  :aliases {"test-all-profiles" ["with-profile" "dev:1.5:1.6:1.7" "test"]
+                   :dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:resource-paths ["test-resources"]
+                   :dependencies [[org.clojure/clojure "1.8.0"]]}}
+  :aliases {"test-all-profiles" ["with-profile" "dev:1.5:1.6:1.7:1.8" "test"]
             "cloverage" ["do" "cloverage" "--output" "doc/coverage"]
             "release" ["do" "clean," "deploy" "clojars"]})
